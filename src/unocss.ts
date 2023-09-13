9c093402a461ed066188e6b5594cae7cf0dd978e
Gerald <code@gera2ld.space>
2023-09-13 16:13:22 +0800

    feat: add injectStyle for unocss

--- src/unocss.ts
+++ src/unocss.ts
@@ -19,4 +19,44 @@ export const initialize = memoize(
   },
 );
 
+async function transformTokens(tokens: string[]) {
+  await initialize();
+  const { uno } = (window as any).__unocss_runtime;
+  // Copied from @unocss/postcss
+  const style = (await Promise.all(tokens.map((i) => uno.parseToken(i, '-'))))
+    .filter(Boolean)
+    .flat()
+    .sort((a, b) => a[0] - b[0])
+    .sort(
+      (a, b) =>
+        (a[3] ? uno.parentOrders.get(a[3]) ?? 0 : 0) -
+        (b[3] ? uno.parentOrders.get(b[3]) ?? 0 : 0),
+    )
+    .map((a) => a[2])
+    .join('');
+  return style;
+}
+
+async function buildCSSItem(selector: string, tokenStr: string) {
+  const style = await transformTokens(tokenStr.split(' ').filter(Boolean));
+  return `${selector}{${style}}`;
+}
+
+export async function buildCSS(shortcuts: Record<string, string>) {
+  const items = await Promise.all(
+    Object.entries(shortcuts).map(([selector, tokenStr]) =>
+      buildCSSItem(selector, tokenStr),
+    ),
+  );
+  return items.join('');
+}
+
+export async function injectStyle(shortcuts: Record<string, string>) {
+  const css = await buildCSS(shortcuts);
+  const el = document.createElement('style');
+  el.textContent = css;
+  document.head.append(el);
+  return el;
+}
+
 setTimeout(initialize);
