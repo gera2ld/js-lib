e0b8a65022db01795e16dfd2983e43361de68e64
Gerald <code@gera2ld.space>
2026-01-02 10:50:58 +0900

    fix: use wind4 by default

--- src/unocss.browser.ts
+++ src/unocss.browser.ts
@@ -1,23 +1,41 @@
-import { loadCSS, loadJS } from './lib/util/index.browser.ts';
+import { loadJS } from './lib/util/index.browser.ts';
 
 interface IUnocssOptions {
   unocss?: any;
-  init?: {
-    reset?: string;
+  prepare?: (
+    defaultPrepare?: (options?: IUnocssOptions) => Promise<void>,
+  ) => Promise<void>;
+}
+
+async function defaultPrepare(options?: IUnocssOptions) {
+  await Promise.all([
+    loadJS(
+      `https://cdn.jsdelivr.net/npm/@unocss/runtime@${__versions__.unocssRuntime}/preset-wind4.global.js`,
+    ),
+    loadJS(
+      `https://cdn.jsdelivr.net/npm/@unocss/runtime@${__versions__.unocssRuntime}/preset-icons.global.js`,
+    ),
+  ]);
+  window.__unocss = {
+    presets: [
+      () => window.__unocss_runtime.presets.presetWind4(),
+      () =>
+        window.__unocss_runtime.presets.presetIcons({
+          scale: 1.2,
+          cdn: 'https://esm.sh/',
+        }),
+    ],
+    ...window.__unocss,
+    ...options?.unocss,
   };
 }
 
-function doInitialize(options?: IUnocssOptions) {
-  let reset = options?.init?.reset ?? 'normalize';
-  if (reset) {
-    if (!reset.includes('://'))
-      reset = `https://cdn.jsdelivr.net/npm/@unocss/reset@${__versions__.unocssReset}/${reset}.min.css`;
-    loadCSS(reset);
-  }
-
-  if (options?.unocss) window.__unocss = options.unocss;
-  return loadJS(
-    `https://cdn.jsdelivr.net/npm/@unocss/runtime@${__versions__.unocssRuntime}`,
+async function doInitialize(options?: IUnocssOptions) {
+  await (options?.prepare
+    ? options.prepare(defaultPrepare)
+    : defaultPrepare(options));
+  await loadJS(
+    `https://cdn.jsdelivr.net/npm/@unocss/runtime@${__versions__.unocssRuntime}/core.global.js`,
   );
 }
 
@@ -28,60 +46,60 @@ export function initialize(options?: IUnocssOptions) {
   return initialized;
 }
 
-async function transformTokens(tokens: string[]) {
-  await initialize();
-  const { uno } = window.__unocss_runtime;
-  // Copied from @unocss/postcss
-  const styles = (await Promise.all(tokens.map((i) => uno.parseToken(i, '-'))))
-    .filter(Boolean)
-    .flat()
-    .sort((a, b) => a[0] - b[0])
-    .sort(
-      (a, b) =>
-        (a[3] ? (uno.parentOrders.get(a[3]) ?? 0) : 0) -
-        (b[3] ? (uno.parentOrders.get(b[3]) ?? 0) : 0),
-    )
-    .reduce<Record<string, string>>(
-      (acc, item) => {
-        const key = `${item[1]}\n${item[3] || ''}`;
-        acc[key] = (acc[key] || '') + item[2];
-        return acc;
-      },
-      {} as Record<string, string>,
-    );
-  return styles;
-}
+// async function transformTokens(tokens: string[]) {
+//   await initialize();
+//   const { uno } = window.__unocss_runtime;
+//   // Copied from @unocss/postcss
+//   const styles = (await Promise.all(tokens.map((i) => uno.parseToken(i, '-'))))
+//     .filter(Boolean)
+//     .flat()
+//     .sort((a, b) => a[0] - b[0])
+//     .sort(
+//       (a, b) =>
+//         (a[3] ? (uno.parentOrders.get(a[3]) ?? 0) : 0) -
+//         (b[3] ? (uno.parentOrders.get(b[3]) ?? 0) : 0),
+//     )
+//     .reduce<Record<string, string>>(
+//       (acc, item) => {
+//         const key = `${item[1]}\n${item[3] || ''}`;
+//         acc[key] = (acc[key] || '') + item[2];
+//         return acc;
+//       },
+//       {} as Record<string, string>,
+//     );
+//   return styles;
+// }
 
-async function buildCSSItem(selector: string, tokenStr: string) {
-  const styles = await transformTokens(tokenStr.split(' ').filter(Boolean));
-  return Object.entries(styles).map(([key, css]) => {
-    const [rawSelector, parent] = key.split('\n');
-    const newSelector = rawSelector
-      .replace(/\s\$\$\s+/g, ' ')
-      .replace(/\.\\-(?:[^-\w]|$)/g, selector);
-    css = `${newSelector}{${css}}`;
-    if (parent) css = `${parent}{${css}}`;
-    return css;
-  });
-}
+// async function buildCSSItem(selector: string, tokenStr: string) {
+//   const styles = await transformTokens(tokenStr.split(' ').filter(Boolean));
+//   return Object.entries(styles).map(([key, css]) => {
+//     const [rawSelector, parent] = key.split('\n');
+//     const newSelector = rawSelector
+//       .replace(/\s\$\$\s+/g, ' ')
+//       .replace(/\.\\-(?:[^-\w]|$)/g, selector);
+//     css = `${newSelector}{${css}}`;
+//     if (parent) css = `${parent}{${css}}`;
+//     return css;
+//   });
+// }
+//
+// export async function buildCSSFromShortcuts(shortcuts: Record<string, string>) {
+//   const items = await Promise.all(
+//     Object.entries(shortcuts).map(([selector, tokenStr]) =>
+//       buildCSSItem(selector, tokenStr),
+//     ),
+//   );
+//   return items.flat().join('\n');
+// }
 
-export async function buildCSSFromShortcuts(shortcuts: Record<string, string>) {
-  const items = await Promise.all(
-    Object.entries(shortcuts).map(([selector, tokenStr]) =>
-      buildCSSItem(selector, tokenStr),
-    ),
-  );
-  return items.flat().join('\n');
-}
-
-export async function injectStyleByShortcuts(
-  shortcuts: Record<string, string>,
-) {
-  const css = await buildCSSFromShortcuts(shortcuts);
-  const el = document.createElement('style');
-  el.textContent = css;
-  document.head.append(el);
-  return el;
-}
+// export async function injectStyleByShortcuts(
+//   shortcuts: Record<string, string>,
+// ) {
+//   const css = await buildCSSFromShortcuts(shortcuts);
+//   const el = document.createElement('style');
+//   el.textContent = css;
+//   document.head.append(el);
+//   return el;
+// }
 
 setTimeout(initialize);
